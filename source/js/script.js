87117d865e9eabc2d0d14ce603080844742bb7ac
Svetlana <svetlanakrbva@rambler.ru>
2020-05-13 15:53:18 +0300

    сделана валидация формы

--- source/js/script.js
+++ source/js/script.js
@@ -6,37 +6,64 @@ var navItem = document.querySelectorAll('.nav__item');
 var navLink = document.querySelectorAll('.nav__link');
 var logoImg = document.querySelector('.logo__img');
 var logoHamburger = document.querySelector('.logo__hamburger');
+var form = document.getElementById('form');
 
+function ready() {
+  function openHamburger() {
+    hamburger.classList.toggle('logo__hamburger--open');
 
-function openHamburger() {
-  hamburger.classList.toggle('logo__hamburger--open');
+    nav.classList.toggle('nav__list--adaptiv');
 
-  nav.classList.toggle('nav__list--adaptiv');
+    navItem.forEach(function (item) {
+      if (item.classList.contains !== ('nav__item--adaptiv')) {
+        item.classList.add('nav__item--adaptiv');
+      }
+    });
 
-  navItem.forEach(function (item) {
-    if (item.classList.contains !== ('nav__item--adaptiv')) {
-      item.classList.add('nav__item--adaptiv');
-    }
-  });
+    navLink.forEach(function (item) {
+      if (item.classList.contains !== ('nav__link--adaptiv')) {
+        item.classList.add('nav__link--adaptiv');
+      }
+    });
+  }
+
+  function closeHamburger() {
+    hamburger.classList.remove('logo__hamburger--open');
+    nav.classList.remove('nav__list--adaptiv');
+  }
+
+  nav.classList.remove('nav__list--nojs');
+  logoImg.style.padding = '20px';
+  logoHamburger.style.display = 'block';
+
+  hamburger.addEventListener('click', openHamburger);
 
   navLink.forEach(function (item) {
-    if (item.classList.contains !== ('nav__link--adaptiv')) {
-      item.classList.add('nav__link--adaptiv');
+    item.addEventListener('click', closeHamburger);
+  });
+
+  $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+  $('#form').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 5
+      },
+      phone: {
+        required: true,
+      }
+    },
+    messages: {
+      name: {
+        required: 'Поле имя обязательно для заполнения',
+        minlength: jQuery.validator.format('Длина имени должна быть больше {0} символов')
+      },
+      phone: {
+        required: 'Введите номер телефона',
+      }
     }
   });
 }
 
-function closeHamburger() {
-  hamburger.classList.remove('logo__hamburger--open');
-  nav.classList.remove('nav__list--adaptiv');
-}
-
-nav.classList.remove('nav__list--nojs');
-logoImg.style.padding = '20px';
-logoHamburger.style.display = 'block';
-
-hamburger.addEventListener('click', openHamburger);
-
-navLink.forEach(function (item) {
-  item.addEventListener('click', closeHamburger);
-});
+document.addEventListener('DOMContentLoaded', ready);
